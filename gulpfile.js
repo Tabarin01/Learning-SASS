096acf76c5ed80aba9820f2a25da8992510bddef
Tabarin01 <119614157+Tabarin01@users.noreply.github.com>
2023-06-23 10:17:42 +0200

    lesson 4 ended

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 const { src, dest, watch, series } = require("gulp"); // import source, destination, watch, series functions
 const sass = require("gulp-sass")(require("sass")); // import gulp-sass plug-in che richiede a sua volta un'altra funzione (compilatore sass)
 
-function buildStyles() { // compiling sass into css
-  return src("index.scss").pipe(sass()).pipe(dest("css"));
+function buildStyles() {
+  // compiling sass into css
+  return src("*.scss").pipe(sass()).pipe(dest("css"));
   // prende index.scss, lo inserisce e lo converte in css
 }
 
-function watchTask() { // funzione per leggere il file sas per le modifiche da ricompilare ad ogni cambiamento
-  watch(["index.scss"], buildStyles);
+function watchTask() {
+  // funzione per leggere il file sas per le modifiche da ricompilare ad ogni cambiamento
+  watch(["*.scss"], buildStyles);
   //invoca watch  e come argomento gli passiamo ciò che vogliamo leggere
   // e come secondo argomento la funzione compilatore
 }
 
-exports.default = series(buildStyles, watchTask); // esportiamo in ordine e poi su terminale:  gulp 
+exports.default = series(buildStyles, watchTask); // esportiamo in ordine e poi su terminale:  gulp
